4887d2a2d4f4d9ec3f1e2f6a4f5dd6f9e11b00ad
kinjal-savitara <kinjal@savitarainfotel.com>
2026-05-11 16:03:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -101,7 +101,7 @@ export default function Home() {
              )}
 
     
-        {!showForm&& ( <button
+        {!showForm && iframeUrl != 'https://biganto.com/tour/48214/?iframe=1' && ( <button
            onClick={() => setShowForm(true)}
            style={{
              position: "absolute",
